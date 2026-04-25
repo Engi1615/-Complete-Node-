8847bebf4d612804c8d3f1ecd721681e9ca2b15c
Engi1615 <kr.sajan1615@gmail.com>
2026-04-25 17:14:49 +0530

    Add shebang to specify program to run file .

---
+++ What is Shebang/app.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+let num = 0;
+setInterval(() => {
+  console.log(num++);
+}, 500);
+
+// at the top is shebang that tells the system which program should be used to execute this scripts file
